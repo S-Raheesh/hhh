23d1933be85ba26281ba638d292a24a966bb3599
S-Raheesh <80512594+S-Raheesh@users.noreply.github.com>
2021-09-12 15:37:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,35 +3,35 @@ ctx= canvas.getContext("2d");
 color = "blue";
 ctx.beginPath();
 ctx.strokeStyle = color;
-ctx.lineWidth=2;
+ctx.lineWidth=10;
 ctx.arc(250,210,40,0,2*Math.PI);
 ctx.stroke();
 
 color = "black";
 ctx.beginPath();
 ctx.strokeStyle = color;
-ctx.lineWidth=2;
+ctx.lineWidth=10;
 ctx.arc(350,210,40,0,2*Math.PI);
 ctx.stroke();
 
 color = "red";
 ctx.beginPath();
 ctx.strokeStyle = color;
-ctx.lineWidth=2;
+ctx.lineWidth=10;
 ctx.arc(450,210,40,0,2*Math.PI);
 ctx.stroke();
 
 color = "yellow";
 ctx.beginPath();
 ctx.strokeStyle = color;
-ctx.lineWidth=2;
+ctx.lineWidth=10;
 ctx.arc(300,250,40,0,2*Math.PI);
 ctx.stroke();
 
 color = "green";
 ctx.beginPath();
 ctx.strokeStyle = color;
-ctx.lineWidth=2;
+ctx.lineWidth=10;
 ctx.arc(400,250,40,0,2*Math.PI);
 ctx.stroke();
 
